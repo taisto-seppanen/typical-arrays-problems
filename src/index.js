9bf26d0ec18d999b630d861118596cf6a3678e4a
taisto-seppanen <taistopetrseppanen@gmail.com>
2021-03-03 17:25:30 +0300

    feat: added answer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
+exports.min = function min(array) {
+    if (!array || array.length == 0) {
+        return 0;
+    }
+    return Math.min(...array);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array || array.length == 0) {
+        return 0;
+    }
+    return Math.max(...array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!array || array.length == 0) {
+        return 0;
+    }
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    return array.reduce(reducer) / array.length;
+};
